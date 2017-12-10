43d0af2440331ebf8ab50c9602e9920586ac6288
Postboredom <noah1010@me.com>
2017-12-09 18:26:55 -0800

    Different Search options

--- dog_stuff.py
+++ dog_stuff.py
@@ -3,7 +3,7 @@ import os
 # Signed_in = [("ADmin","Test1234")]
 Animal_classes = []
 file_Name = open("static/Dogs.txt")
-file_Name2 = open("static/people.txt",encoding="utf8")
+#file_Name2 = open("static/people.txt",encoding="utf8")
 #==============================================================
 #Class for keeping all the Animal data in
 class Animal:
@@ -39,8 +39,8 @@ def Save_Animals():
     fileObject.close()
 
 #Creates a new animal and pits it in the Animal_list
-def New_Animal(Animal_type, Animal_name,Type,Age,Location,Color,Picture):
-    Animal_type = Animal(Animal_name,Age,Type,Location,Color,Picture)
+def New_Animal( Animal_name,Type,Age,Location,Color,Picture):
+    Animal_type = Animal( Animal_name,Age,Type,Location,Color,Picture)
     Animal_classes.append(Animal_type)
 
 #Finds all animals in the Animal_list
