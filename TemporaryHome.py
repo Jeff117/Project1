4198a2fbd4ef07f967eb1fbc1349df6a335aa073
Jeff117 <33636576+Jeff117@users.noreply.github.com>
2017-12-09 20:17:41 -0800

    Update TemporaryHome.py

--- TemporaryHome.py
+++ TemporaryHome.py
@@ -1,20 +1,26 @@
 # Author: Jeff San Paolo
 # Description: Homepage used as a template to ensure my pages work as designed
-# $env: FLASK_APP = "TemporaryHome.py"
-# $nv:FLASK_DEBUG = "1"
+# $env:FLASK_APP = "TemporaryHome.py"
+# $env:FLASK_DEBUG = "1"
 # flask run
 
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for,request,redirect
 from flask_bootstrap import Bootstrap
 from PIL import Image
-
+from NewHome import *
 app = Flask(__name__)
 Bootstrap(app)
 
-@app.route('/Search')
-def SearchAnimals():
-    return render_template('SearchAnimals.html')
 
 @app.route('/Report')
 def ReportAnimals():
     return render_template('ReportAnimals.html')
+
+@app.route('/Search')
+def SearchAnimals():
+        return render_template('SearchAnimals.html')
+
+@app.route('/Searchs', methods = ['POST'])
+def SearchAnimal(dogname):
+    return render_template('SearchAnimals.html',
+    Animallist = Animal_search(dogname[0],dogname[1],dogname[2]), doit = "yes")
